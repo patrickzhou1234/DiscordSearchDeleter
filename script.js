bc29bb55819936d277d9d9f3e2f1fbe981194bed
Patrick Zhou <patrickchang44543@gmail.com>
2023-06-26 21:10:18 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,59 @@
+function deleteMsgs() {
+    var i = 0;
+    var idInt = setInterval(() => {
+        fetch(
+            "https://discord.com/api/v9/channels/" +
+                url[5] +
+                "/messages/" +
+                id[i],
+            {
+                headers: {
+                    accept: "*/*",
+                    "accept-language": "en-US,en;q=0.9",
+                    authorization: (webpackChunkdiscord_app.push([
+                        [""],
+                        {},
+                        (e) => {
+                            m = [];
+                            for (let c in e.c) m.push(e.c[c]);
+                        },
+                    ]),
+                    m)
+                        .find((m) => m?.exports?.default?.getToken !== void 0)
+                        .exports.default.getToken(),
+                },
+                referrerPolicy: "strict-origin-when-cross-origin",
+                body: null,
+                method: "DELETE",
+                mode: "cors",
+                credentials: "include",
+            }
+        );
+        i++;
+        if (i == id.length) clearInterval(idInt);
+    }, 1000);
+}
+
 const url = window.location.href.split("/");
 var id = [];
 document.querySelectorAll(".searchResult-O9NDji").forEach((el) => {
-        id.push(el.childNodes[0].childNodes[0].id.split("-")[2]);
+    id.push(el.childNodes[0].childNodes[0].id.split("-")[2]);
 });
 if (!document.querySelectorAll(".endButton-pLBGXH")[1].disabled) {
-  	var sInt = setInterval(() => {
+    var sInt = setInterval(() => {
         document.querySelectorAll(".searchResult-O9NDji").forEach((el) => {
-              id.push(el.childNodes[0].childNodes[0].id.split("-")[2]);
+            id.push(el.childNodes[0].childNodes[0].id.split("-")[2]);
         });
+        if (document.querySelectorAll(".endButton-pLBGXH")[1].disabled) {
+            clearInterval(sInt);
+            deleteMsgs();
+        }
     }, 1530);
     var fInt = setInterval(() => {
         if (document.querySelectorAll(".endButton-pLBGXH")[1].disabled) {
             clearInterval(fInt);
-          	clearInterval(sInt);
         } else {
             document.querySelectorAll(".endButton-pLBGXH")[1].click();
         }
     }, 1500);
 }
-var i = 0;
-var idInt = setInterval(() => {
-    fetch("https://discord.com/api/v9/channels/" + url[5] + "/messages/" + id[i], {
-        headers: {
-            accept: "*/*",
-            "accept-language": "en-US,en;q=0.9",
-            authorization: (webpackChunkdiscord_app.push([
-                [""],
-                {},
-                (e) => {
-                    m = [];
-                    for (let c in e.c) m.push(e.c[c]);
-                },
-            ]),
-            m)
-                .find((m) => m?.exports?.default?.getToken !== void 0)
-                .exports.default.getToken(),
-        },
-        referrerPolicy: "strict-origin-when-cross-origin",
-        body: null,
-        method: "DELETE",
-        mode: "cors",
-        credentials: "include",
-    });
-    i++;
-    if (i == id.length) clearInterval(idInt);
-}, 1000);
